d463f6cd658da1425205fc4656932a53c4c2621c
Anuj Pandey <anujpandey1701@yahoo.com>
2025-02-05 13:52:15 +0530

    Implemented async functions with setTimeout, setInterval, and fetch API with error handling.

---
+++ asynchronous.js
@@ -0,0 +1,45 @@
+function message(message,delay){
+    setTimeout(function(){
+        console.log(message);
+    },delay)
+}
+
+let userInput = prompt('Enter Message:').trim();
+let userDelay = Number(prompt('Enter Delay eg: 1000 is 1sec').trim());
+message(userInput,userDelay);
+
+function countDown(number){
+    let interval = setInterval(function(){
+        console.log(number);
+        if(number===0){
+            clearInterval(interval);
+            console.log('Count Down is complete!');
+        }
+        number--;
+    },1000);
+}
+
+let userInput2 = Number(prompt('Enter Number for countdown').trim());
+if(isNaN(userInput2)){
+    console.log('pls enter a valid no :(');
+}
+else{
+    countDown(userInput2);
+}
+
+
+async function fetchData() {
+    try{
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        if(!response.ok){
+            throw new Error("Failed to fetch data");
+        }
+        let data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log("Error",error.message);
+    }
+}
+
+fetchData();
